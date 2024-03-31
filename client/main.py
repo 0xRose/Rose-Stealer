8461441f8552e3290ab3a7646c510dab7e4679e8
gumbobrot <96620548+gumbobrot@users.noreply.github.com>
2024-03-31 01:46:11 +0100

    Update main.py

--- client/main.py
+++ client/main.py
@@ -377,7 +377,7 @@ class Config:
         base_path = os.path.abspath(".")
 
     def __init__(self) -> None:
-        key = "eb0abca617432754f19ad3b0f02c44ad041709bfba8fdb18c87463459394bff8"  # hexified 32 byte key (128-bit)
+        key = "AES_KEY"  # hexified 32 byte key (128-bit)
         config_file_name = "config.aes"  # config file path like config.aes, configuration.dat or conf.ini
 
         self.config = configparser.ConfigParser()
